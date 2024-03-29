0ec6ad049b19a32c7fb7da1b59e219985dda3230
Weiss Jiang <meweissjiang@gmail.com>
2024-03-29 21:06:52 +1100

    imported commonModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { WishItem } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'wishlist';
+  title = 'my wishlist';
+  items = [
+    new WishItem('To learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Find grass that cuts itseld')
+  ]
 }
